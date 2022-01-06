5eac7697f8f2d0f075b8c876c9bb4d622bed847b
Juan Rendon <juanbits@gmail.com>
2022-01-06 03:48:42 +0100

    Se incluye djangorestframework

--- truehome/settings.py
+++ truehome/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'thjcrm'
+    'thjcrm',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
